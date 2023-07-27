a727884195dc547970831a8b39921c6a393ff7f3
George Pogrebnoi <gosha1416@yandex.ru>
2023-07-27 12:35:30 +0300

    Added pagination to endpoints datasets/ and datasets/<pk>/data

--- csv_access/views.py
+++ csv_access/views.py
@@ -1,10 +1,21 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 
 from .models import Dataset
 from .serializers import DatasetListSerializer, DatasetSerializer
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    """
+    Standard pagination settings class
+    """
+
+    page_size = 100
+    page_size_query_param = "page_size"
+    max_page_size = 200
 
 
 class DatasetViewSet(viewsets.ModelViewSet):
@@ -17,18 +28,25 @@ class DatasetViewSet(viewsets.ModelViewSet):
     queryset = Dataset.objects.all()
 
     def list(self, request):
-        serializer = DatasetListSerializer(self.get_queryset(), many=True)
-        return Response(serializer.data)
+        """List Datasets with pagination"""
+        pagination = StandardResultsSetPagination()
+        page = pagination.paginate_queryset(self.get_queryset(), request)
+        serializer = DatasetListSerializer(page, many=True)
+        return pagination.get_paginated_response(serializer.data)
 
     @action(methods=["post"], detail=False, url_path="create", url_name="create-dataset")
     def new(self, request):
+        """Upload new Dataset"""
         return self.create(request)
 
     @action(methods=["get"], detail=True, url_path="data", url_name="get-data")
     def get_data(self, request, pk):
+        """List Dataset data with pagination"""
         if pk:
             dataset = Dataset.objects.get(pk=pk)
             if dataset:
-                print(dataset)
-                return Response(dataset.get_data())
-        return Response(404)
+                data = dataset.get_data()
+                pagination = StandardResultsSetPagination()
+                page = pagination.paginate_queryset(data, request)
+                return pagination.get_paginated_response(page)
+        return Response(status.HTTP_404_NOT_FOUND)
--- file_api/settings.py
+++ file_api/settings.py
@@ -127,3 +127,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination"}
